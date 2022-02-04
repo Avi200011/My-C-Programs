dca776eaf6cf054e913688c0bccf70e93b0802d1
Avijit sarkar <avhijitsarkar3@gmail.com>
2022-02-05 00:05:29 +0530

    add a cout

--- hackerrank/problem1.c
+++ hackerrank/problem1.c
@@ -1,16 +1,17 @@
-#include<stdio.h>
-#include<conio.h>
+#include <stdio.h>
+#include <conio.h>
 
-int main(){
-    int n=5;
+int main()
+{
+    int n = 5;
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j<n; j++)
+        for (int j = 0; j < n; j++)
         {
-            printf("%d " ,n);
+            printf("%d ", n);
         }
         printf("\n");
     }
-    
+    cout<<"changes";
     return 0;
 }
