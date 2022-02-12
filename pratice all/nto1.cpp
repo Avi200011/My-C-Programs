8d2eb5854519eaff266a73726b7dd39e069517ad
Avijit sarkar <avhijitsarkar3@gmail.com>
2022-02-12 22:33:04 +0530

    push

--- pratice all/nto1.cpp
+++ pratice all/nto1.cpp
@@ -1,1 +1,20 @@
-#include<bi
+#include<bits/stdc++.h>
+using namespace std;
+void print(int num)
+{
+    if (num==1)
+    {
+        cout<<num<<" ";
+        return;
+    }
+    cout<<num<<" ";
+    print(num-1);
+    
+
+}
+int main()
+{
+    int n=10;
+    print(n);
+    return 0;
+}
