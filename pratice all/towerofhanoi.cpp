eb6d6ddfaa439d4eaf3bc8f958989c83d6bf307d
Avijit sarkar <avhijitsarkar3@gmail.com>
2022-02-16 16:02:02 +0530

    push

--- pratice all/towerofhanoi.cpp
+++ pratice all/towerofhanoi.cpp
@@ -1,17 +1,18 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
-void toh(int n,int a,int b,int c)
+void toh(int n, int a, int b, int c)
 {
-    if (n>0){
-        toh(n-1,a,c,b);
-        cout<<a<<c<<endl;
-        toh(n-1,b,a,c) ;
-    } 
-    
+    if (n == 0)
+    {
+        return;
+    }
+    toh(n - 1, a, c, b);
+    cout << "move " << a << " to " << c << endl;
+    toh(n - 1, b, a, c);
 }
 
 int main()
 {
-    toh(3,1,2,3);
+    toh(3, 1, 2, 3);
     return 0;
 }
