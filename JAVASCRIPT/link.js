69ae16cef23d09e5493580f221b3905ecb433d87
Avijit sarkar <avhijitsarkar3@gmail.com>
2022-04-03 19:03:56 +0530

    jspush

---
+++ JAVASCRIPT/link.js
@@ -0,0 +1,31 @@
+// let js = "amazing";
+// if (js === "amazing") alert("lets do the java boss");
+
+console.log(30 + 20 - 10);
+console.log("happy");
+
+let firstname = "Avijit";
+console.log(firstname);
+
+let myCourse = "mechnical";
+let myDream = "programming";
+console.log(myCourse);
+console.log(myDream);
+
+//bolline value of the
+let javascript = true;
+console.log(javascript);
+console.log(typeof javascript);
+console.log(typeof 69);
+console.log(typeof 'happpy');
+javascript="fun";
+console.log(javascript);
+
+let year;
+console.log(year);
+console.log(typeof year);
+year=2000;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
