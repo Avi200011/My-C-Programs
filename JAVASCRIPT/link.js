d4dd54d641d07c5c2d8927b0de4ad8fe91dc2561
Avijit sarkar <avhijitsarkar3@gmail.com>
2022-05-14 12:46:30 +0530

    push

--- JAVASCRIPT/link.js
+++ JAVASCRIPT/link.js
@@ -63,28 +63,96 @@ const jonesHeight = 1.88;
 
 const bmimark = marksWeight / markHeight ** 2;
 const bmijones = jonesWeight / (jonesHeight / jonesWeight);
-console.log(bmijones,bmimark);
-console.log(bmijones<bmimark);
+console.log(bmijones, bmimark);
+console.log(bmijones < bmimark);
 
 console.log(" ADD MULTIPLE STRING ");
-const name='kavin'
-const job='programmer';
-const Byear='2000';
-const Cyear='2020';
-const kavin=  "I'm " + firstname + ',my age '+(Cyear-Byear)+' and i am a '+ job ;
+const name = 'kavin'
+const job = 'programmer';
+const Byear = '2000';
+const Cyear = '2020';
+const kavin = "I'm " + firstname + ',my age ' + (Cyear - Byear) + ' and i am a ' + job;
 console.log(kavin);
 console.log(`have
 a
 great journey`);
-const age=14;
-if (age >=18) {
+const age = 14;
+if (age >= 18) {
     console.log("Dost you can enjoy ");
 
-}else{[
-    console.log("sorry bro")
-]}
+} else {
+    [
+        console.log("sorry bro")
+    ]
+}
+
+//type conversion
 const testyear = '2000';
 console.log(Number(testyear));
-console.log(Number(testyear)+18);
+console.log(Number(testyear) + 18);
 console.log(Number('ballu'));
-console.log(typeof(NaN));
+console.log(typeof (NaN));
+console.log(String(23), 23);
+console.log(typeof (String(23)));
+console.log(23);
+
+//type coresion
+console.log('I am ' + 21 + ' years old '); //convert num to string
+console.log('23' + 11 + '11'); //but this is all strings
+console.log('23' - 11 - '11'); //For "-" all string convert to num
+let n = '1' + 1;
+n = n - 1;
+console.log(n);
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('console'));
+console.log(Boolean(({})));
+console.log(Boolean(''));
+
+const money = 0;
+if (money) {
+    console.log("this is false ")
+} else {
+    console.log("This is false so this is will be output")
+}
+let undefinedname = 0;
+if (undefinedname) {
+    console.log("Bro this is not output");
+} else {
+    console.log("this is the output");
+}
+
+//const chack = Number(prompt("tyoe a number "));
+// console.log(chack);
+// console.log(typeof chack);
+// if (chack === 56) {
+//     console.log("This is the number");
+// }
+
+const hascoding = false;
+const hasskill = true;
+
+console.log(hascoding && hasskill);
+console.log(hascoding || hasskill);
+
+const day = 'satarday';
+switch (day) {
+    case 'satarday': // day===console call ;
+        console.log('starting of all day');
+        break;
+    case 'tuesday':
+        console.log('visit to manali');
+        break;
+    case 'wednesday':
+    case 'friday':
+        console.log('time to stay with manali');
+        break;
+    case 'satarday':
+        console.log('half day');
+    case 'sunday':
+        console.log('this is the fun day');
+    default:
+        console.log('not a valid day');
+        break;
+}
