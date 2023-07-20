ae0064a527db405383d4cc5fd6fcff577703ef7a
Avijit sarkar <avhijitsarkar3@gmail.com>
2023-07-20 12:59:45 +0530

    go

---
+++ gfg_Array/sum0.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+void findtriplets(int arr[],int n){
+    bool found=false;
+    for(int i=0;i<n-2;i++){
+        for(int j=i+1;j<n-1;j++){
+            for(int k=j+1;k<n;k++){
+                if(arr[i]+arr[j]+arr[k]==0){
+                    cout<<arr[i]<<" "<<arr[j]<<" "<<arr[k]<<endl;
+                    found=true;
+                }
+            }
+        }
+    }
+    if(found==false){
+        cout<<"not exsist";
+    }
+}
+int main(){
+    int arr[]={0,-1,2,-3,1};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    findtriplets(arr,n);
+    return 0;
+}
