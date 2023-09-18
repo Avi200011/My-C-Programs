9592c73babc36bf4ce80d256097f44ffe8fd4a8b
Avijit sarkar <avhijitsarkar3@gmail.com>
2023-09-19 01:34:52 +0530

    new

---
+++ String/declear_string.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string word[10];
+    word[0]="hello";
+    word[1]="world";
+    for (int i = 0; i < 5; i++)
+    {
+        cout<<word[i]<<" ";
+    }
+    return 0;
+}
